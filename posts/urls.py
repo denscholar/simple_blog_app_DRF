dcf0d294a8705085e90b297d47ba7be632e1b65e
Dennis Akagha <d.akagha20@gmail.com>
2023-08-18 13:23:22 +0100

    add class apiview code

--- posts/urls.py
+++ posts/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("homepage/", views.list_post, name="posts"),
-    path("post_detail/<int:pk>/", views.post_detail, name="post_detail"),
-    path("update/<int:pk>/", views.update_post, name="post_update"),
-    path("delete/<int:pk>/", views.delete_post, name="post_delete"),
+    # path("homepage/", views.list_post, name="posts"),
+    # path("post_detail/<int:pk>/", views.post_detail, name="post_detail"),
+    # path("update/<int:pk>/", views.update_post, name="post_update"),
+    # path("delete/<int:pk>/", views.delete_post, name="post_delete"),
+
+    # classbased views url endpoints
+    path("", views.PostListCreate.as_view(), name="post_list_create"),
+    path("post_detail/<int:id>/", views.PostRetrieveUppdateAPIView.as_view(), name="post_detail"),
+
+
+
 ]
